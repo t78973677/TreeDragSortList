db7a5a20831dbf8db595012fe8cfe9fa92b8c97b
Jain Zang <t78973677@yahoo.com.tw>
2015-05-30 12:00:43 +0800

    Initial commit

---
+++ app/src/androidTest/java/org/jain/treedragsortlist/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.jain.treedragsortlist;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
